09f61354441ecd215ddbc73b601b656bd3e74f73
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2024-06-03 15:42:00 +0200

    slight tweak to project structure

--- benchmark/uuid_factory_b.cpp
+++ benchmark/uuid_factory_b.cpp
@@ -1,15 +1,21 @@
+#include <uuid.h>
+#include <uuid_factory.h>
 #include <benchmark/benchmark.h>
 
-static void BM_SomeFunction(benchmark::State& state) {
+static void BM_create_batch(benchmark::State& state) {
+    using namespace LambdaSnail::Uuid;
 
-    for (auto _ : state) {
-        int x = 5;
-        int y = 10;
-        int z = x + y;
-    }
+    auto const num_uuids = static_cast<uint16_t>(state.range(0));
+    std::vector<uuid> uuid_vec;
+    factory::create_uuids_fbdc(num_uuids, uuid_vec);
+
+    // for(uuid const& uuid: uuid_vec)
+    // {
+    //     std::cout << uuid.as_string() << std::endl;
+    // }
 }
-// Register the function as a benchmark
-BENCHMARK(BM_SomeFunction);
 
-// Run the benchmark
+BENCHMARK(BM_create_batch)->Arg(256);
+//BENCHMARK(BM_create_batch)->Iterations(1000000)->Arg(256);
+
 BENCHMARK_MAIN();

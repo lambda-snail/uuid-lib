f714718d1d8a79824b0b020f2977cd231c094a55
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2024-06-03 14:06:20 +0200

    fix error in factory function

--- include/uuid_factory.h
+++ include/uuid_factory.h
@@ -36,19 +36,13 @@ namespace LambdaSnail::Uuid
                 uuid& uuid = out_vec.emplace_back(0);
                 spec.set_ts_ms(uuid.m_octets, time_stamp);
                 spec.set_rand_a(uuid.m_octets, i);
-
-                uint64_t rand_b = random_generator.next();
-                spec.set_rand_b(uuid.m_octets, ++rand_b);
             }
 
             for(int i = 0; i < num_uuids; ++i)
             {
                 uint64_t rand_b = random_generator.next();
-                spec.set_rand_b(out_vec[i].m_octets, ++rand_b);
+                spec.set_rand_b(out_vec[i].m_octets, rand_b);
             }
-
-            //memcpy(octets.data(), &time_stamp, sizeof(int64_t));
         }
-
     }
 }
